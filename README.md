b8582463ec54693ee3c463e06a5ed5e2bbabd71f
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-04-22 20:07:01 -0700

    add jest tests

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # SVG-Logo-Maker
 8th assignment of UCB Extension Web Development Bootcamp
+
+## Desciption
+This is a computer program that you can use by typing commands into a special window. When you use the program, it will ask you for some information. You can type up to three letters, choose a color, and select a shape from a list of options. Once you've provided all the information, the program will create a picture called "logo.svg" and display a message that says "Generated logo.svg". You can open the picture in a web browser and it will be 300 pixels wide and 200 pixels tall. It will look like the picture you described when you gave the program all the information.
+
+______________
+
+## Technology Used 
+   
+* [Git](https://git-scm.com/)   
+* [JavaScript](https://www.javascript.com/)   
+* [Node.js](https://nodejs.dev/)
+
+## Requirements
+Run
+
+```
+npm install
+```
+
+
+## Author Info
+
+### Liubov Sobolevskaya
+* [LinkedIn](https://www.linkedin.com/in/liubov-sobolevskaya-45756a101/)
+* [Github](https://github.com/LiubovSobolevskaya)
+* [Kaggle](https://www.kaggle.com/lyubovsobolevskaya)
+
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {generateSVG} = require('./lib/generateSVG');
+const questions = [{ 
+                    message: "Please enter up to three charachters",
+                    type: "input",
+                    name: "charachters",
+                    validate: function(input) { 
+                        return input.length <= 3;
+                    }
+                   }, 
+                   {
+                    message:  "Please enter text color",
+                    type: "input",
+                    name: "color",
+                   },
+                   {
+                    message:  "Please enter a shape",
+                    type: "list",
+                    name: "shape",
+                    choices: ['circle', 'triangle', 'square']
+                   },
+                   {
+                    message: "Please enter shape's color",
+                    type: "input",
+                    name: "shapeColor",
+                   },
+                ];
+
+
+
+function writeToFile(fileName, data) {
+    text = generateSVG(data);
+    fs.writeFile(fileName, text, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+}
+function init() {
+   inquirer
+  .prompt(questions)
+  .then((data) => {
+    const fileName = 'examples/logo.svg';
+    writeToFile(fileName, data);
+  });
+
+}
+
+
+init();
+
